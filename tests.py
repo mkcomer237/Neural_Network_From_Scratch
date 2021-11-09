a99cbac1395f6137c1246c78d4ca17c8cfc7b87f
mkcomer237 <mkcomer@gmail.com>
2021-11-09 09:44:48 -0500

    update formatting

--- tests.py
+++ tests.py
@@ -5,9 +5,9 @@ import numpy as np
 from NeuralNetNLayer import NeuralNet
 
 
-A=np.array([[1, -1, -3, -1, 1, 1], 
-            [2, 2, 2, 2, 2, 2],
-            [2, 0, 1, 0, 3, 3]])
+A = np.array([[1, -1, -3, -1, 1, 1], 
+             [2, 2, 2, 2, 2, 2],
+             [2, 0, 1, 0, 3, 3]])
 
 b = np.array([1, 0, 0, 0, 1, 1]).reshape(1, 6)
 
